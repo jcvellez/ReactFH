de7b78117995407dd54b01ed6a6aa5ab8fd1e251
jcvellez <jcvellez@live.com>
2023-01-15 13:16:21 -0500

    Me toca clase 2 Parte 1 Seccion 5

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ numero }) => {
-    console.log(numero);
+
+    let [COUNTER, setCOUNTER] = useState(numero);
+   
+    let handleAdd = () => {          
+        setCOUNTER(COUNTER+1);
+        //setCOUNTER((c)=>c+1);
+        //COUNTER+1     
+    };
+    let handleSubstract = () => {          
+        setCOUNTER(COUNTER-1);
+        //setCOUNTER((c)=>c+1);
+        //COUNTER+1     
+    };
+    let handleReset = () => {          
+        setCOUNTER(COUNTER=numero);
+        //setCOUNTER((c)=>c+1);
+        //COUNTER+1     
+    };
+
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{numero}</h2>
+            <h2>{COUNTER}</h2>
+            <button onClick={handleAdd}>+1</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleSubstract}>-1</button>
         </>
     );
 };
 
-
-CounterApp.propTypes={
+CounterApp.propTypes = {
     numero: PropTypes.number.isRequired
 }
 
