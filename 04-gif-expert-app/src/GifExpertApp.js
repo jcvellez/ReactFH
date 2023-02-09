4534d8f70fd90fb93cb8731c993c6100453baeb4
jcvellez <jcvellez@live.com>
2023-02-08 23:20:21 -0500

    Me toca clase 6 Parte 1 Seccion 6

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const GifExpertApp = () => {
-
+const GifExpertApp = () => {    
+    const [categories, setcategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+    
+    let handleAdd = () => {          
+        //setcategories(['Hunter Galaxy',...categories] );
+        setcategories(cats=> [...cats, 'Hunter Galaxy'] );
+    };
+    
+    
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr></hr>
+            <button onClick={handleAdd}>Agregar</button>
+            <ol>
+                {
+                    categories.map((category)=>{
+                        return <li key={category}> {category }</li>
+                    })
+                }
+            </ol>
         </>
     );
-
 }
 
 export default GifExpertApp;
