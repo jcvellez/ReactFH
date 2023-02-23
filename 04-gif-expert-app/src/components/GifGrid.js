f8421dbed0bf44db3289b2b8b03bcd7f4ed1e204
jcvellez <jcvellez@live.com>
2023-02-22 22:55:24 -0500

    Me toca clase 11 Parte 1 Seccion 6

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,17 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-    const [count, setcount] = useState(0);
+    let [count, setcount] = useState(0);
+    let [images, setimages] = useState([]);
     useEffect(() => {
         getGifs();
     }, [])
-    
+
     const getGifs = async () => {
         const url1 = 'https://api.giphy.com/v1/gifs/search?q=cheeseburgers&limit=10&api_key=vkLsiWGxnOyN6sruCs0lwMMRgdJ5Z2tM';
-        const respuesta = await fetch( url1 );
-        const {data} = await respuesta.json();
-        
-        const gifs = data.map((img)=>{
+        const respuesta = await fetch(url1);
+        const { data } = await respuesta.json();
+
+        const gifs = data.map((img) => {
             return {
                 id: img.id,
                 title: img.title,
@@ -20,14 +22,28 @@ export const GifGrid = ({ category }) => {
         })
 
         console.log(gifs);
+        setimages(gifs);
+
     }
 
     //getGifs();
     return (
         <div>
             <h3>{category}</h3>
-            <h3>{count} </h3>
+            <ol>
+                {
+                    images.map( img => (
+                        <GifGridItem
+                            key={img.id }
+                            {...img}
+                        />
+                        // <li key={id}>{title}</li>
+                    ))
+                }
+            </ol>
+            {/* <h3>{count} </h3>
             <button onClick={ ()=>setcount(count + 1) }>dale</button>
+            <button onClick={ ()=>setcount(count = 0 ) }>reset</button> */}
         </div>
     )
 }
